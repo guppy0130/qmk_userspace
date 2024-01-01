412263f1501e6457ebdcd28adfe2b847d63aa0ec
guppy0130 <guppy0130@yahoo.com>
2024-01-01 10:18:31 -0800

    fixup oled flickering issue

--- keyboards/crkbd/keymaps/guppy0130/config.h
+++ keyboards/crkbd/keymaps/guppy0130/config.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef OLED_ENABLE
 #   define SPLIT_OLED_ENABLE
+#   define OLED_TIMEOUT 10000
 #endif
 
 #ifdef WPM_ENABLE
